9baeace89fab4b238893e1a13ada45a6e06c81cf
Yuan Zhu <y_zhu@apple.com>
2016-03-02 14:38:00 -0800

    GroupedBarChart initial commit.

---
+++ CareKit/Dashboard/OCKGroupedBarChartView.h
@@ -0,0 +1,52 @@
+//
+//  OCKAdherenceChartView.h
+//  CareKit
+//
+//  Created by Yuan Zhu on 2/29/16.
+//  Copyright © 2016 carekit.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class OCKGroupedBarChartView;
+@protocol OCKGroupedBarChartViewDataSource <NSObject>
+
+- (NSUInteger)numberOfGroupsInChartView:(OCKGroupedBarChartView *)chartView;
+
+- (NSUInteger)numberOfBarsPerGroupInChartView:(OCKGroupedBarChartView *)chartView;
+
+- (UIColor *)chartView:(OCKGroupedBarChartView *)chartView colorForBar:(NSUInteger)barIndex;
+
+- (NSString *)chartView:(OCKGroupedBarChartView *)chartView nameForBar:(NSUInteger)barIndex;
+
+- (NSNumber *)chartView:(OCKGroupedBarChartView *)chartView valueForBar:(NSUInteger)barIndex inGroup:(NSUInteger)groupIndex;
+
+- (nullable NSString *)chartView:(OCKGroupedBarChartView *)chartView stringForBar:(NSUInteger)barIndex inGroup:(NSUInteger)groupIndex;
+
+- (nullable NSString *)chartView:(OCKGroupedBarChartView *)chartView titleForGroup:(NSUInteger)groupIndex;
+
+@optional
+
+- (NSString *)chartView:(OCKGroupedBarChartView *)chartView textForGroup:(NSUInteger)groupIndex;
+
+@end
+
+
+@interface OCKGroupedBarChartView : UIView
+
+/*
+ All the information for charting are provided by OCKGroupedBarChartViewDataSource.
+ */
+@property (nonatomic, weak, null_resettable) id<OCKGroupedBarChartViewDataSource> dataSource;
+
+
+/*
+ Show filling animation of all the bars.
+ */
+- (void)animationWithDuration:(NSTimeInterval)duration;
+
+@end
+
+NS_ASSUME_NONNULL_END

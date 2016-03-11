d36fc1704b4d45e3c9e85488d0ece537cc7817f5
Yuan Zhu <y_zhu@apple.com>
2016-03-10 16:38:14 -0800

    Update documentation for PDF printing.

--- CareKit/PDF/OCKDocument.h
+++ CareKit/PDF/OCKDocument.h
@@ -1,23 +1,50 @@
-//
-//  OCKDocument.h
-//  CareKit
-//
-//  Created by Yuan Zhu on 2/23/16.
-//  Copyright © 2016 carekit.org. All rights reserved.
-//
+/*
+ Copyright (c) 2016, Apple Inc. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <Foundation/Foundation.h>
 #import <CareKit/CareKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-/*
+/**
  OCKDocumentElement defines the protocol to be adopted by all the elements in OCKDocument.
  It requires an element to output to HTML content.
  Developer can create custom element confirming to this protocol to be inlcuded in a document.
  */
 @protocol OCKDocumentElement <NSObject>
 
+/**
+ Content in HTML format.
+ 
+ @return HTML content.
+ */
 - (NSString *)HTMLContent;
 
 @end
@@ -28,83 +55,128 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface OCKDocument : NSObject
 
+/**
+ Initializer of OCKDocument.
+ 
+ @param title       Title of the document.
+ @param elements    Elements to be included in the document.
+ 
+ @return    Initialized OCKDocument instance.
+ */
 - (instancetype)initWithTitle:(NSString *)title elements:(NSArray<id<OCKDocumentElement>> *)elements;
 
 /**
- Title of the document.
+ Title of the document; printed in large font at the beginging of the file.
  */
 @property (nonatomic, copy, nullable) NSString *title;
 
 /**
- pageHeader will be printed in every PDF page.
+ pageHeader will be printed in every PDF page above other content.
  It can be used to help identify the source of a file.
  For example, @"App Name: ABC, User ID: 123456";
  */
 @property (nonatomic, copy, nullable) NSString *pageHeader;
 
 /**
- Included elements.
+ Elements in the document.
  */
 @property (nonatomic, copy, nullable) NSArray<id <OCKDocumentElement>> *elements;
 
 /**
+ Document content in HTML format.
  */
 @property (nonatomic, copy, readonly) NSString *HTMLContent;
 
 /**
- Create PDF from current document object.
+ Create a PDF file based on current document object.
  */
 - (void)createPDFWithCompletion:(void (^)(NSData *data, NSError *error))completion;
 
 @end
 
+
 /**
- Defines an element carries a subtitle in document.
+ Defines a subtitle element to be included in document.
  */
 @interface OCKDocumentElementSubtitle : NSObject <OCKDocumentElement>
 
+/**
+ Initializer of OCKDocumentElementSubtitle.
+ 
+ @param subtitle    Subtitle string.
+ @return    Initialized OCKDocumentElementSubtitle instance.
+ */
 - (instancetype)initWithSubtitle:(NSString *)subtitle;
 
+/**
+ Subtitle string.
+ */
 @property (nonatomic, copy, nullable) NSString *subtitle;
 
 @end
 
 
 /**
- Defines an element carries a paragraph of text in document.
+ Defines an text paragraph element to be included in document.
  */
 @interface OCKDocumentElementParagraph : NSObject <OCKDocumentElement>
 
+/**
+ Initializer of OCKDocumentElementParagraph.
+ 
+ @param     content    Paragraph content string.
+ @return    Initialized OCKDocumentElementParagraph instance.
+ */
 - (instancetype)initWithContent:(NSString *)content;
 
+/**
+ Paragraph content string.
+ */
 @property (nonatomic, copy, nullable) NSString *content;
 
 @end
 
 
 /**
- Defines an element carries an image object to be included in a document.
+ Defines an image element to be included in a document.
  */
 @interface OCKDocumentElementImage : NSObject <OCKDocumentElement>
 
+/**
+ Initializer of OCKDocumentElementImage.
+ 
+ @param     image    Image object.
+ @return    Initialized OCKDocumentElementImage instance.
+ */
 - (instancetype)initWithImage:(UIImage *)image;
 
+/**
+ Image object.
+ */
 @property (nonatomic, strong, nullable) UIImage *image;
 
 @end
 
 
 /**
- Defines an element carries a chart to be included in a document as image.
+ Defines an chart element to be included in a document.
+ The title and text of the chart will be included in document as well.
  */
 @interface OCKDocumentElementChart : NSObject <OCKDocumentElement>
 
+/**
+ Initializer of OCKDocumentElementChart.
+ 
+ @param     chart    OCKChart object.
+ @return    Initialized OCKDocumentElementChart instance.
+ */
 - (instancetype)initWithChart:(OCKChart *)chart;
 
+/**
+ OCKChart object.
+ */
 @property (nonatomic, strong, nullable) OCKChart *chart;
 
 @end
-
-
 
 NS_ASSUME_NONNULL_END
--- CareKit/PDF/OCKDocument.m
+++ CareKit/PDF/OCKDocument.m
@@ -1,14 +1,38 @@
-//
-//  OCKDocument.m
-//  CareKit
-//
-//  Created by Yuan Zhu on 2/23/16.
-//  Copyright © 2016 carekit.org. All rights reserved.
-//
+/*
+ Copyright (c) 2016, Apple Inc. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 #import "OCKDocument.h"
 #import "OCKChart_Internal.h"
 #import "OCKHTMLPDFWriter.h"
+
 
 @implementation OCKDocument {
     OCKHTMLPDFWriter *_writer;
@@ -69,6 +93,7 @@
 
 @end
 
+
 @implementation OCKDocumentElementSubtitle 
 
 - (instancetype)initWithSubtitle:(NSString *)subtitle {
@@ -91,6 +116,7 @@
 
 @end
 
+
 @implementation OCKDocumentElementParagraph
 
 - (instancetype)initWithContent:(NSString *)content {
@@ -111,6 +137,7 @@
 
 @end
 
+
 static NSString *imageTagFromImage (UIImage *image) {
     NSData *imageData = UIImagePNGRepresentation(image);
     NSString *format = @"<img style='vertical-align: middle;' alt=\"\" height='%@' width='%@' src='data:image/png;base64,%@' />\n";
@@ -126,6 +153,7 @@ static NSString *imageTagFromView (UIView *view) {
     UIGraphicsEndImageContext();
     return imageTagFromImage(image);
 }
+
 
 @implementation OCKDocumentElementChart
 

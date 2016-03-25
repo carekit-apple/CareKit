9065aa71f805c785c39421a30787d7bdc6cb4cb1
Umer Khan <umer_khan@apple.com>
2016-03-24 17:06:00 -0700

    Sample app

---
+++ sample/OCKSample/OCKSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCKSample
+//
+//  Created by Umer Khan on 3/24/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/OCKSample/OCKSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCKSample
+//
+//  Created by Umer Khan on 3/24/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

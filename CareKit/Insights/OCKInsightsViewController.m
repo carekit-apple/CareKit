d1de066f99958297503387a681c5a81fa61b6279
Umer Khan <umer_khan@apple.com>
2016-03-28 10:09:35 -0700

    Refactored insights.

--- CareKit/Insights/OCKInsightsViewController.m
+++ CareKit/Insights/OCKInsightsViewController.m
@@ -30,13 +30,26 @@
 
 
 #import "OCKInsightsViewController.h"
+#import "OCKInsightsTableViewHeaderView.h"
+#import "OCKInsightsChartTableViewCell.h"
+#import "OCKInsightsMessageTableViewCell.h"
 #import "OCKChart.h"
-#import "OCKInsightsTableViewController.h"
 #import "OCKHelpers.h"
 
 
+static const CGFloat HeaderViewHeight = 60.0;
+static const CGFloat TopMargin = 20.0;
+
+@interface OCKInsightsViewController() <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+
 @implementation OCKInsightsViewController {
-    OCKInsightsTableViewController *_tableViewController;
+    UITableView *_tableView;
+    OCKInsightsTableViewHeaderView *_headerView;
+    NSMutableArray *_constraints;
+    BOOL _hasAnimated;
 }
 
 - (instancetype)init {
@@ -50,26 +63,132 @@
     self = [super init];
     if (self) {
         _items = [items copy];
+        _hasAnimated = NO;
         
-        _tableViewController = [[OCKInsightsTableViewController alloc] initWithInsightItems:_items];
-        [self addChildViewController:_tableViewController];
-        [self.view addSubview:_tableViewController.view];
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleGrouped];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        [self.view addSubview:_tableView];
+        
+        [self setUpConstraints];
     }
     return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if (!_headerView) {
+        _headerView = [[OCKInsightsTableViewHeaderView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, HeaderViewHeight)];
+    }
+    _headerView.title = _headerTitle;
+    _headerView.subtitle = _headerSubtitle;
+    _tableView.tableHeaderView = _headerView;
+    
+    _tableView.estimatedRowHeight = 44.0;
+    _tableView.sectionHeaderHeight = 5.0;
+    _tableView.sectionFooterHeight = 0.0;
+    _tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 - (void)setItems:(NSArray<OCKInsightItem *> *)items {
     NSAssert(items.count > 0, @"OCKInsightsViewController requires at least one insight item.");
     _items = [items copy];
-    _tableViewController.items = _items;
+    [_tableView reloadData];
 }
 
 - (void)setHeaderTitle:(NSString *)headerTitle {
-    _tableViewController.headerTitle = headerTitle;
+    _headerView.title = headerTitle;
 }
 
 - (void)setHeaderSubtitle:(NSString *)headerSubtitle {
-    _tableViewController.headerSubtitle = headerSubtitle;
+    _headerView.subtitle = headerSubtitle;
+}
+
+- (void)setUpConstraints {
+    [NSLayoutConstraint deactivateConstraints:_constraints];
+    
+    _constraints = [NSMutableArray new];
+    
+    _tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [_constraints addObjectsFromArray:@[
+                                        [NSLayoutConstraint constraintWithItem:_tableView
+                                                                     attribute:NSLayoutAttributeTop
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeTop
+                                                                    multiplier:1.0
+                                                                      constant:TopMargin],
+                                        [NSLayoutConstraint constraintWithItem:_tableView
+                                                                     attribute:NSLayoutAttributeBottom
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeBottom
+                                                                    multiplier:1.0
+                                                                      constant:0.0],
+                                        [NSLayoutConstraint constraintWithItem:_tableView
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeLeading
+                                                                    multiplier:1.0
+                                                                      constant:0.0],
+                                        [NSLayoutConstraint constraintWithItem:_tableView
+                                                                     attribute:NSLayoutAttributeTrailing
+                                                                     relatedBy:NSLayoutRelationEqual
+                                                                        toItem:self.view
+                                                                     attribute:NSLayoutAttributeTrailing
+                                                                    multiplier:1.0
+                                                                      constant:0.0]
+                                        ]];
+    
+    [NSLayoutConstraint activateConstraints:_constraints];
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return _items.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    OCKInsightItem *item = _items[indexPath.section];
+    
+    if ([item isKindOfClass:[OCKChart class]]) {
+        static NSString *ChartCellIdentifier = @"ChartCell";
+        OCKInsightsChartTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ChartCellIdentifier];
+        if (!cell) {
+            cell = [[OCKInsightsChartTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                        reuseIdentifier:ChartCellIdentifier];
+        }
+        cell.chart = (OCKChart *)item;
+        return cell;
+    } else if ([item isKindOfClass:[OCKMessageItem class]]) {
+        static NSString *MessageCellIdentifier = @"MessageCell";
+        OCKInsightsMessageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MessageCellIdentifier];
+        if (!cell) {
+            cell = [[OCKInsightsMessageTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                          reuseIdentifier:MessageCellIdentifier];
+        }
+        cell.messageItem = (OCKMessageItem *)item;
+        return cell;
+    }
+    
+    return nil;
 }
 
 @end

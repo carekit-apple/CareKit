88bff833fb02b05292b8092993885bc67951f864
Erik Hornberger <hornbergererik@gmail.com>
2016-12-03 20:53:00 +0900

    Updated copyright notice

--- CareKit/CareCard/OCKCareCardViewController.m
+++ CareKit/CareCard/OCKCareCardViewController.m
@@ -1,6 +1,7 @@
 /*
  Copyright (c) 2016, Apple Inc. All rights reserved.
  Copyright (c) 2016, Troy Tsubota. All rights reserved.
+ Copyright (c) 2016, Erik Hornberger. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:

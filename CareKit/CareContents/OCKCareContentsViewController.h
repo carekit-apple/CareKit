fdcd34c3b767111dab60700a532168e12bce02de
Erik Hornberger <hornbergererik@gmail.com>
2018-01-27 12:52:44 +0900

    Update argument names and fixed incorrect selector on Care Contents

--- CareKit/CareContents/OCKCareContentsViewController.h
+++ CareKit/CareContents/OCKCareContentsViewController.h
@@ -144,19 +144,19 @@ NS_ASSUME_NONNULL_BEGIN
  Asks the delegate for text to display below each `OCKCareCardButton` in the cell when in a selected state.
  
  @param viewController              The view controller providing the callback.
- @param interventionActivity        The `OCKCarePlanEvent` that the button represents
+ @param carePlanEvent               The `OCKCarePlanEvent` that the button represents
  @param index                       The index of the `OCKCareCardButton` within the cell
  */
-- (nullable NSString *)careContentsViewController:(OCKCareContentsViewController *)viewController selectedStateTextForInterventionActivity: (OCKCarePlanEvent *)interventionActivity atButtonIndex: (NSInteger)index;
+- (nullable NSString *)careContentsViewController:(OCKCareContentsViewController *)viewController selectedStateTextForCarePlanEvent: (OCKCarePlanEvent *)carePlanEvent atButtonIndex: (NSInteger)index;
 
 /**
  Asks the delegate for text to display below each `OCKCareCardButton` in the cell when in a deselected state.
  
  @param viewController              The view controller providing the callback.
- @param interventionActivity        The `OCKCarePlanEvent` that the button represents
+ @param carePlanEvent               The `OCKCarePlanEvent` that the button represents
  @param index                       The index of the `OCKCareCardButton` within the cell
  */
-- (nullable NSString *)careContentsViewController:(OCKCareContentsViewController *)viewController deselectedStateTextForInterventionActivity: (OCKCarePlanEvent *)interventionActivity atButtonIndex: (NSInteger)index;
+- (nullable NSString *)careContentsViewController:(OCKCareContentsViewController *)viewController deselectedStateTextForCarePlanEvent: (OCKCarePlanEvent *)carePlanEvent atButtonIndex: (NSInteger)index;
 
 @end
 
--- CareKit/CareContents/OCKCareContentsViewController.m
+++ CareKit/CareContents/OCKCareContentsViewController.m
@@ -948,16 +948,16 @@
     }
 }
 
-- (nullable NSString*)careCardTableViewCell:(OCKCareCardTableViewCell *)cell selectedStateTextForInterventionActivity:(OCKCarePlanEvent *)interventionActivity atIndex:(NSInteger)index {
-    if ([self.delegate respondsToSelector:@selector(careCardTableViewCell:selectedStateTextForInterventionActivity:atIndex:)]){
-        return [self.delegate careContentsViewController:self selectedStateTextForInterventionActivity:interventionActivity atButtonIndex:index];
+- (nullable NSString*)careCardTableViewCell:(OCKCareCardTableViewCell *)cell selectedStateTextForCarePlanEvent:(OCKCarePlanEvent *)carePlanEvent atIndex:(NSInteger)index {
+    if([self.delegate respondsToSelector:@selector(careContentsViewController:selectedStateTextForCarePlanEvent:atButtonIndex:)]) {
+        return [self.delegate careContentsViewController:self selectedStateTextForCarePlanEvent:carePlanEvent atButtonIndex:index];
     }
     return NULL;
 }
 
-- (nullable NSString*)careCardTableViewCell:(OCKCareCardTableViewCell *)cell deselectedStateTextForInterventionActivity:(OCKCarePlanEvent *)interventionActivity atIndex:(NSInteger)index {
-    if ([self.delegate respondsToSelector:@selector(careCardTableViewCell:deselectedStateTextForInterventionActivity:atIndex:)]){
-        return [self.delegate careContentsViewController:self deselectedStateTextForInterventionActivity:interventionActivity atButtonIndex:index];
+- (nullable NSString*)careCardTableViewCell:(OCKCareCardTableViewCell *)cell deselectedStateTextForCarePlanEvent:(OCKCarePlanEvent *)carePlanEvent atIndex:(NSInteger)index {
+    if([self.delegate respondsToSelector:@selector(careContentsViewController:deselectedStateTextForCarePlanEvent:atButtonIndex:)]) {
+        return [self.delegate careContentsViewController:self deselectedStateTextForCarePlanEvent:carePlanEvent atButtonIndex:index];
     }
     return NULL;
 }

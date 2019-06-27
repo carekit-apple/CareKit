a855f47c04ffbb154767366ea58fc4031af1577b
Erik Hornberger <erik_h@apple.com>
2019-06-26 18:04:15 -0700

    Added Packakge.swift file

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "CareKit",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "CareKit",
+            targets: ["CareKit"]),
+        
+        .library(
+            name: "CareKitUI",
+            targets: ["CareKitUI"]),
+        
+        .library(
+            name: "CareKitStore",
+            targets: ["CareKitStore"])
+    ],
+    targets: [
+        .target(
+            name: "CareKit",
+            dependencies: ["CareKitUI", "CareKitStore"],
+            path: "CareKit/CareKit"),
+        
+        .target(
+            name: "CareKitUI",
+            path: "CareKitUI/CareKitUI"),
+        
+        .target(
+            name: "CareKitStore",
+            path: "CareKitStore/CareKitStore"),
+        
+        .testTarget(
+            name: "CareKitStoreTests",
+            dependencies: ["CareKitStore"],
+            path: "CareKitStore/CareKitStoreTests"),
+        
+        .testTarget(
+            name: "CareKitTests",
+            dependencies: ["CareKit"],
+            path: "CareKit/CareKitTests")
+    ]
+)

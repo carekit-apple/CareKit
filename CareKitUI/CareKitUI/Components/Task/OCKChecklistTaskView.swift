616d6eaeb39bdc1c4b5bde847c49c8871a192958
Gavi Rawson <gavirawson@gmail.com>
2019-07-09 14:12:27 -0400

    Make body text multiline.
    Body text in the cards are now multiline.

--- CareKitUI/CareKitUI/Components/Task/OCKChecklistTaskView.swift
+++ CareKitUI/CareKitUI/Components/Task/OCKChecklistTaskView.swift
@@ -122,6 +122,7 @@ open class OCKChecklistTaskView: UIView, OCKCardable, OCKCollapsible, OCKCollaps
     public let instructionsLabel: OCKLabel = {
         let label = OCKLabel(textStyle: .caption1, weight: .regular)
         label.textColor = .lightGray
+        label.numberOfLines = 0
         return label
     }()
     

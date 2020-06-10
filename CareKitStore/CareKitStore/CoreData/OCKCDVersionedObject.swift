e25f6da1e59e4011f53fc69ddf313410b57d1a47
Corey MacBook Pro <coreyearleon@icloud.com>
2020-06-10 19:15:16 -0400

    "next" is being updated somewhere, reverting changes

--- CareKitStore/CareKitStore/CoreData/OCKCDVersionedObject.swift
+++ CareKitStore/CareKitStore/CoreData/OCKCDVersionedObject.swift
@@ -38,7 +38,7 @@ class OCKCDVersionedObject: OCKCDObject {
     @NSManaged var allowsMissingRelationships: Bool
     @NSManaged var effectiveDate: Date
     @NSManaged var deletedDate: Date?
-    @NSManaged var next: OCKCDVersionedObject?
+    @NSManaged private(set) weak var next: OCKCDVersionedObject?
 
     var nextVersionUUID: UUID? {
         return next?.uuid

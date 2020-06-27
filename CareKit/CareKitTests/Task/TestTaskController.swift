6c3126eea595f510f87d6fd24cbfb899b1049fe6
Corey MacBook Pro <coreyearleon@icloud.com>
2020-06-27 16:10:10 -0400

    bump failing testcase by a second

--- CareKit/CareKitTests/Task/TestTaskController.swift
+++ CareKit/CareKitTests/Task/TestTaskController.swift
@@ -278,7 +278,7 @@ class TestTaskController: XCTestCase {
         let expectations = try setupDeletedTaskNotificationTest(controller: controller)
 
         controller.fetchAndObserveEvents(forTaskIDs: ["doxylamine"], eventQuery: .init(for: Date()))
-        wait(for: expectations, timeout: 2)
+        wait(for: expectations, timeout: 3)
     }
 
     func testFetchWithTaskQueryReceivesDeletedTaskNotifications() throws {

4bf7069a059374e1dfd8c07976243510ed09006e
Dylan Li <dylanli859@gmail.com>
2020-07-13 18:58:08 -0400

    changed default values in OCKSliderTaskController

--- CareKit/CareKit/iOS/Task/Controllers/OCKSliderTaskController.swift
+++ CareKit/CareKit/iOS/Task/Controllers/OCKSliderTaskController.swift
@@ -31,16 +31,23 @@ open class OCKSliderTaskController: OCKTaskController {
     private func makeViewModel(from taskEvents: OCKTaskEvents) -> SliderTaskViewModel? {
         guard !taskEvents.isEmpty else { return nil }
         
-        let event = taskEvents.first?.first
-        var value: CGFloat = 0
-        var isComplete = false
-        
-        if let foundValue = event?.scheduleEvent.element.targetValues.first?.numberValue?.doubleValue {
-            value = CGFloat(foundValue)
-            isComplete = true
+        let errorHandler: (Error) -> Void = { [weak self] error in
+            self?.error = error
         }
 
-        return .init(title: taskEvents.firstEventTitle, detail: taskEvents.firstEventDetail, instructions: taskEvents.firstTaskInstructions, isComplete: isComplete, action: {}, maximumImage: nil, minimumImage: nil, initialValue: value, range: (value - 5)...(value + 5), step: 1, sliderHeight: 40, frameHeightMultiplier: 1.7, useDefaultSlider: false)
+        return .init(title: taskEvents.firstEventTitle,
+                     detail: taskEvents.firstEventDetail,
+                     instructions: taskEvents.firstTaskInstructions,
+                     isComplete: taskEvents.isFirstEventComplete,
+                     action: toggleActionForFirstEvent(errorHandler: errorHandler),
+                     maximumImage: nil,
+                     minimumImage: nil,
+                     initialValue: 5,
+                     range: 0...10,
+                     step: 1,
+                     sliderHeight: 40,
+                     frameHeightMultiplier: 1.7,
+                     useDefaultSlider: false)
     }
         
 }

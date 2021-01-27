d4fca9a3a61d8d3fb693329254a138613dbb9837
Corey's iMac <coreyearleon@icloud.com>
2021-01-27 18:45:01 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -37,8 +37,7 @@ let package = Package(
 
         .target(
             name: "CareKitStore",
-            path: "CareKitStore/CareKitStore",
-            resources: [.process("CoreData/CareKitStore.xcdatamodel")]),
+            path: "CareKitStore/CareKitStore"),
 
         .target(
             name: "CareKitFHIR",
